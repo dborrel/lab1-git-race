6d89b7233a1610ea1d45e7096f7564ee5a320cdf
dborrel <871643@unizar.es>
2025-09-24 08:36:23 +0200

    Feature: implement caching for frequently accessed data

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -3,7 +3,10 @@ package es.unizar.webeng.hello
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
+
+@EnableCaching
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class Application
